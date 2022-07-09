7825d8b669342e0a5b2760628a597cf3b733eb2f
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-08 22:44:27 -0700

    added sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,28 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const SideBar = () => {
     return(
         <div className="nav-bar">
-            <h3>Sidebar</h3>
             <Link className='logo' to={'/'}>
                 <img src={LogoS} alt="logo" />
-                <img src={LogoSubtitle} alt="logo" />
+                <img className='sub-logo' src={LogoSubtitle} alt="logo" />
             </Link>
+            <nav>
+                <NavLink className='home-link' exact="true" activeclassname="active" to='/'>
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to='/about'>
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
         </div>
     )
 }
