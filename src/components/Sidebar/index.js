9e00b05c4427b62f5d047c6c6539668c5e58e488
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-10 18:06:45 -0700

    added social media brand icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const SideBar = () => {
     return(
@@ -23,6 +24,35 @@ const SideBar = () => {
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a 
+                        href='https://www.linkedin.com/in/luis-gonzalez-a33799235/'
+                        rel='noreferrer'
+                        target={"_blank"}
+                    >
+                        <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href='https://www.github.com/Skinwalker423'
+                        rel='noreferrer'
+                        target={"_blank"}
+                    >
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        href='https://www.twitter.com/artgonzalez423'
+                        rel='noreferrer'
+                        target={"_blank"}
+                    >
+                        <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
