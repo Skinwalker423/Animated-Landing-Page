efd2be4e372cc7f703e56a08a5e2d9bd30472fd0
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-11 17:14:17 -0700

    rubberband animations on hover

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import '../../index.scss';
 import './home-styles.scss';
 import AnimatedLetters from '../../components/AnimatedLetters';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
 
@@ -12,13 +12,27 @@ const Home = () => {
     const jobTitleArray = 'Web Developer.'.split('');
 
     const [letterClass, setLetterClass] = useState('text-animate');
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('hover')
+        }, 4000)
+
+    }, [])
     
 
 
     return(
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi, <br/>I'm
+                <h1>
+                   <span className={letterClass}>H</span>
+                   <span className={`${letterClass} _12`}>i,</span>
+                   <br/>
+                   <span className={`${letterClass} _13`}>I</span>
+                   <span className={`${letterClass} _14`}>'m</span>
+                 
                  <img src={LogoTitle} alt='developer' />
                 <AnimatedLetters
                     letterClass={letterClass}
@@ -27,7 +41,7 @@ const Home = () => {
                 />
                 <br />
                 <AnimatedLetters
-                    letterClass
+                    letterClass={letterClass}
                     strArray={jobTitleArray}
                     idx={22}
                  />
